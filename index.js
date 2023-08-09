bb665c0148d1d67608fc552d1d35d9afa6498ec9
DanielMWwamba <danmuamba81@gmai.com>
2023-08-09 13:11:16 +0000

    add: modifier index.js, modifier index.html, ajoute style.css

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 // Mixed message
 
-// Tableau de données pour chaque partie du message
-const subjects = ["Vous", "Je", "Les gens", "Le monde"];
-const verbs = ["devez", "peut", "doit", "va"];
-const actions = ["apprendre", "explorer", "créer", "aimer"];
-const complements = ["de nouvelles compétences", "de nouvelles possibilités", "de nouvelles idées", "de nouvelles perspectives"];
+const citation = document.getElementById("msg-info");
+const buttonMessage = document.getElementById("btn-msg");
 
+// Tableaux de données pour chaque partie de la citation
 
-function generateRandomMessage() {
-  const subject = subjects[Math.floor(Math.random() * subjects.length)];
-  const verb = verbs[Math.floor(Math.random() * verbs.length)];
-  const action = actions[Math.floor(Math.random() * actions.length)];
-  const complement = complements[Math.floor(Math.random() * complements.length)];
+const beginnings = ["La vie", "Le bonheur", "Le succès", "L'amour"];
+const middles = ["est un voyage", "se trouve dans les petits riens",
+  "s'obtient par la persévérance", "ne connaît pas de limites"];
+const ends = ["qui mérite d'être vécu", "quand on regarde dans la bonne direction",
+  "lorsqu'on y met tout son coeur", "qui peut transformer le monde"];
 
-  const message = subject + " " + verb + " " + action + " " + complement + ".";
+// Fonction de génération de citation aléatoire
 
-  return message;
+function generateRandomQuote() {
+  const beginning = beginnings[Math.floor(Math.random() * beginnings.length)];
+  const middle = middles[Math.floor(Math.random() * middles.length)];
+  const end = ends[Math.floor(Math.random() * ends.length)];
+
+  citation.innerText = ` ${beginning} ${middle} ${end} . `;
+
 }
 
-
-console.log(generateRandomMessage());
-
+// Affichage de la citation générée
+buttonMessage.addEventListener("click", generateRandomQuote)
